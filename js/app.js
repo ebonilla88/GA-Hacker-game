2360dad08e28ec616454199629f8cf075ca86960
github username <github email address>
2020-02-10 14:42:15 -0800

    1st

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log("autosave check")
+// CONSTANTS
+
+//State Variables
+
+
+//CACHED ELEMENTS
+let btn = document.querySelector('#btn');
+
+
+//EVENT LISTENERS
+btn.addEventListener('click', hello);
+
+//Functions
+function hello(){
+  console.log('hello')
+}
