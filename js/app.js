283842d09a85be0a1fac7b2783c5d82e97abc68e
github username <github email address>
2020-02-10 17:43:43 -0800

    command line added

--- js/app.js
+++ js/app.js
@@ -6,12 +6,26 @@ console.log("autosave check")
 
 //CACHED ELEMENTS
 let btn = document.querySelector('#btn');
-
+let input= document.querySelector('#input')
 
 //EVENT LISTENERS
 btn.addEventListener('click', hello);
 
+input.addEventListener('keypress', (event) => {
+  if (event.key == 'Enter') {
+    checkInput(event.target.value);
+  }
+})
+
+
 //Functions
 function hello(){
   console.log('hello')
+}
+function checkInput(str){
+  if(str == "ls"){
+    console("testing the ls responds");
+  } else {
+    alert("not ls....")
+  }
 }
