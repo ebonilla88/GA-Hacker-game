58063331a4d11ba1a9233799ba7d0f3f8a76ed5c
github username <github email address>
2020-02-13 12:28:50 -0800

    working

--- js/app.js
+++ js/app.js
@@ -19,21 +19,21 @@
 // })
 
 const brock = {
-  notes: ["I love LaCroix eh", "2. my second note"],
+  notes: ["I love LaCroix eh"],
   emails: ["kesha      <sent>"],
-  message: [`found this article about react, I know how much you love 'react'`]
+  message: [`I found this article about react, I know how much you love 'react'`]
 }
 
 const kesha = {
-  notes: ["kesha first note", "kesha my second note"],
+  notes: ["Brock drinks way to much LaCroix"],
   emails: ["CEO       <sent>"],
   message: [`Dont forget your password 'GAisAwesome'` ]
 }
 
 const ceo = {
-  notes: ["kesha first note", "kesha my second note"],
+  notes: ["this dude brock loves Goldenrod colored things"],
   emails: ["kesha       <sent>"],
-  message: [`Kesha, what's my password again?` ],
+  message: [`Brock is canadian` ],
   passwords: ["The Matrix"],
 }
 
@@ -66,7 +66,6 @@ let consoleDiv = document.querySelector('.console');
 btn.addEventListener('click', startBtn);
 input.addEventListener('keypress', enterStr);
 
-
 //Functions
 
 function enterStr(event){
@@ -76,33 +75,72 @@ function enterStr(event){
     // before doing ifs, split the input on :, check first value
     const split = event.target.value.split(":")
     if ((split.length > 0) && split[0] === "sei-8-" && split[1] === "help"){
+      addCommandLine("#####################################")
       addCommandLine("Help menu:")
-      addCommandLine("- Help us find CEO's secret message")
+      addCommandLine("- Mission: find CEO's secret message")
       addCommandLine(`- Type 'run' to start hack routine`)
-
-    }else if ((split.length > 0) && split[0] === "sei-8-" && split[1] === "run") {
-      addCommandLine("----------------------- ")
-      addCommandLine("Running hack routine...")
-      addCommandLine("Successfully launched")
-      addCommandLine("-----------------------")
-      addCommandLine("Username List:")
-      addCommandLine(`- Brock 'LaCroix'`)
-      addCommandLine("- Kesha")
-      addCommandLine("- CEO")
-      addCommandLine("Type username")
+      addCommandLine("######################################")
+    } else if ((split.length > 0) && split[0] === "sei-8-" && split[1] === "run") {
+      addCommandLine("|---------------------------|")
+      addCommandLine(`| Running hack routine.....  |`)
+      addCommandLine("|---------------------------|")
+      addCommandLine("| Successfully launched.... |")
+      addCommandLine("|---------------------------|")
       document.querySelector('#input').value = "Username:";
 
+    } else if( (split.length > 0) && split[0] === "Username" && split[1] === "help"){
+        addCommandLine("-------------------")
+        addCommandLine("Username List:")
+        addCommandLine(`- Brock 'LaCroix'`)
+        addCommandLine("- Kesha")
+        addCommandLine("- CEO")
+        addCommandLine("Type username")
+        addCommandLine("-------------------")
+        document.querySelector('#input').value = "Username:";
+
       //#################-brock's logic below-#######################
+
     } else if ((split.length > 0) && split[0] === "Username" && split[1] === "brock" || split[1] === "Brock") {
         document.querySelector('#input').value = "Password:";
 
-    } else if((split.length > 0) && split[0] === "Password" && split[1] === "LaCroix") {
+    } else if((split.length > 0) && split[0] === "Password" && split[1] === "LaCroix" || split[1] === "lacroix") {
         brockProfile()
         document.querySelector('#input').value = "Brock:";
 
-    } else if(event.target.value === "Brock:emails:cd ..") {
+    } else if((split.length > 0) && split[0] === "Brock" & split[1] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Brock:emails' to check emails`);
+        addCommandLine(`- type 'Brock:notes' to check notes`);
+        addCommandLine(`- type 'Brock:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Brock:";
+
+    } else if((split.length > 0) && split[0] === "Brock" && split[1] === "emails" && split[2] === "kesha" && split[3] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Brock:emails:kesha:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Brock:emails:kesha:";
+
+    }else if(event.target.value === "Brock:emails:cd ..") {
         brockProfile()
         document.querySelector('#input').value = "Brock:";
+
+    } else if((split.length > 0) && split[0] === "Brock" && split[1] === "emails" && split[2] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Brock:emails:kesha' to read email`);
+        addCommandLine(`- type 'Brock:emails:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Brock:emails:";
+
+    } else if((split.length > 0) && split[0] === "Brock" && split[1] === "notes" && split[2] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Brock:notes:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Brock:notes:";
 
     } else if (event.target.value == "Brock:emails:kesha:cd ..") {
           addCommandLine(nOutput(brock.emails, "emails"))
@@ -124,27 +162,54 @@ function enterStr(event){
         addCommandLine(nOutput(brock.notes, "notes"))
         document.querySelector('#input').value = "Brock:notes:";
 
-    } else if((split.length > 0) && split[0] === "Brock" && split[1] === "exit"){
-        addCommandLine("Username List:")
-        addCommandLine("- Brock LaCroix")
-        addCommandLine("- Kesha")
-        addCommandLine("- CEO")
-        addCommandLine("Type username")
-        document.querySelector('#input').value = "Username:";
+    } else if((split.length > 0) && split[0] === "Brock" && split[1] === "cd .."){
+      addCommandLine("-------------------")
+      addCommandLine("Username List:")
+      addCommandLine(`- Brock 'LaCroix'`)
+      addCommandLine("- Kesha")
+      addCommandLine("- CEO")
+      addCommandLine("Type username")
+      addCommandLine("-------------------")
+      document.querySelector('#input').value = "Username:";
 
-      //kesha's logic
-    } else if ((split.length > 0) && split[0] === "Username" && split[1] === "kesha") {
+      //####################- kesha's logic -#######################
+    } else if((split.length > 0) && split[0] === "Password" && split[1] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- Go thru other systems to find clues`);
+        addCommandLine("----------------------------------------")
         document.querySelector('#input').value = "Password:";
 
-    } else if((split.length > 0) && split[0] === "Password" && split[1] === "react") {
+    }else if ((split.length > 0) && split[0] === "Password" && split[1] === "cd ..") {
+        document.querySelector('#input').value = "Username:";
+
+    }else if ((split.length > 0) && split[0] === "Username" && split[1] === "kesha") {
+        document.querySelector('#input').value = "Password:";
+
+    } else if((split.length > 0) && split[0] === "Password" && split[1] === "react" || split[1] === "React") {
         keshaProfile()
         document.querySelector('#input').value = "Kesha:";
+
+    } else if((split.length > 0) && split[0] === "Kesha" && split[1] === "emails" && split[2] === "ceo" && split[3] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Kesha:emails:ceo:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Kesha:emails:ceo:";
+
+    }else if((split.length > 0) && split[1] === "emails" && split[2] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Kesha:emails:ceo' to read email`);
+        addCommandLine(`- type 'Kesha:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Kesha:emails:";
 
     } else if(event.target.value === "Kesha:emails:cd ..") {
         keshaProfile()
         document.querySelector('#input').value = "Kesha:";
 
-    } else if (event.target.value == "kesha:emails:ceo:cd ..") {
+    } else if (event.target.value == "kesha:emails:ceo:cd .." || event.target.value == "kesha:emails:CEO:cd .." ) {
           addCommandLine(nOutput(kesha.emails, "emails"))
           document.querySelector('#input').value = "Kesha:emails:";
 
@@ -152,9 +217,26 @@ function enterStr(event){
         addCommandLine(nOutput(kesha.message, "message"))
         document.querySelector('#input').value = "Kesha:emails:ceo:";
 
-    } else if((split.length > 0) && split[0] === "Kesha" && split[1] === "emails") {
+    }  else if((split.length > 0) && split[0] === "Kesha" && split[1] === "emails") {
         addCommandLine(nOutput(kesha.emails, "emails"))
         document.querySelector('#input').value = "Kesha:emails:";
+
+    } else if((split.length > 0) && split[0] === "Kesha" & split[1] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Kesha:emails' to check emails`);
+        addCommandLine(`- type 'Kesha:notes' to check notes`);
+        addCommandLine(`- type 'Kesha:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+        document.querySelector('#input').value = "Kesha:";
+
+    } else if((split.length > 0) && split[1] === "notes" && split[2] === "help"){
+        addCommandLine("----------------------------------------")
+        addCommandLine("Help menu:");
+        addCommandLine(`- type 'Kesha:notes:cd ..' to go back`);
+        addCommandLine("----------------------------------------")
+
+        document.querySelector('#input').value = "Kesha:notes:";
 
     } else if(event.target.value === "Kesha:notes:cd .."){
         keshaProfile()
@@ -164,16 +246,18 @@ function enterStr(event){
         addCommandLine(nOutput(kesha.notes, "notes"))
         document.querySelector('#input').value = "Kesha:notes:";
 
-    } else if((split.length > 0) && split[0] === "Kesha" && split[1] === "exit"){
+    } else if((split.length > 0) && split[0] === "Kesha" && split[1] === "cd .."){
+        addCommandLine("-------------------")
         addCommandLine("Username List:")
-        addCommandLine("- Brock LaCroix")
+        addCommandLine(`- Brock 'LaCroix'`)
         addCommandLine("- Kesha")
         addCommandLine("- CEO")
         addCommandLine("Type username")
+        addCommandLine("-------------------")
         document.querySelector('#input').value = "Username:";
 
       //####################-CEO logic-########################
-    } else if ((split.length > 0) && split[0] === "Username" && split[1] === "ceo") {
+    } else if ((split.length > 0) && split[0] === "Username" && split[1] === "ceo" || split[1] === "CEO") {
         document.querySelector('#input').value = "Password:";
 
     } else if((split.length > 0) && split[0] === "Password" && split[1] === "GAisAwesome" || split[1] === "gaisawesome"){
@@ -191,6 +275,14 @@ function enterStr(event){
     } else if(event.target.value == "CEO:emails:kesha") {
         addCommandLine(nOutput(ceo.message, "message"))
         document.querySelector('#input').value = "CEO:emails:kesha:";
+
+    } else if((split.length > 0) && split[0] === "CEO" && split[1] === "message"){
+        addCommandLine(nOutput(ceo.message, "messages"))
+        document.querySelector('#input').value = "CEO:messages:";
+
+    } else if (event.target.value == "CEO:messages:cd ..") {
+          ceoProfile()
+          document.querySelector('#input').value = "CEO:";
 
     }else if((split.length > 0) && split[0] === "CEO" && split[1] === "emails"){
         addCommandLine(nOutput(ceo.emails, "emails"))
@@ -212,6 +304,13 @@ function enterStr(event){
         addCommandLine("Type username")
         document.querySelector('#input').value = "Username:";
 
+    } else if((split.length > 0) && split[1] === "reset" || split[2]=== "reset" || split[3] === "reset"){
+        addCommandLine("Local System Startup")
+        addCommandLine("Bootup complete")
+        addCommandLine("Version 1.0")
+        addCommandLine("Started: 2/14/2020")
+        addCommandLine(`Type 'help' for help`)
+        addCommandLine(`Type 'run' to start hacking`)
     } else {
         command2.textContent = event.target.value;
         addCommandLine("Command not found");
@@ -228,7 +327,7 @@ function startBtn(){
   addCommandLine("Bootup complete")
   addCommandLine("Version 1.0")
   addCommandLine("Started: 2/14/2020")
-  addCommandLine(`Type 'help' for help`)
+  addCommandLine(`Type 'help' at any point to list of commands`)
   addCommandLine(`Type 'run' to start hacking`)
   // set the innerHTML value of that element to what you want to be displayed
 }
